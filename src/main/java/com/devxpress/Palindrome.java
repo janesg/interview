4a3186389cd9a1750db32f2e3f596d6e60171909
Gary Janes <gary_janes@hotmail.com>
2023-04-16 10:42:05 +0100

    Optimise palindrome check

--- src/main/java/com/devxpress/Palindrome.java
+++ src/main/java/com/devxpress/Palindrome.java
@@ -37,4 +37,17 @@ public class Palindrome {
         return sb.toString().equals(str);
     }
 
+    public static boolean palindromeOptimised(String str) {
+
+        int len = str.length();
+
+        // Optimised by only looping through half the string
+        for (int i = 0; i < len / 2; i++) {
+            if (str.charAt(i) != str.charAt(len - 1 - i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
--- src/test/java/com/devxpress/PalindromeTest.java
+++ src/test/java/com/devxpress/PalindromeTest.java
@@ -14,30 +14,38 @@ import static org.junit.Assert.*;
 public class PalindromeTest {
     @Test
     public void palindromeBuilderReverse() {
-        assertEquals(false, Palindrome.palindromeBuilderReverse("Abba"));
-        assertEquals(true, Palindrome.palindromeBuilderReverse("abba"));
+        assertFalse(Palindrome.palindromeBuilderReverse("Abba"));
+        assertTrue(Palindrome.palindromeBuilderReverse("abba"));
     }
 
     @Test
     public void palindromeForLoop() {
-        assertEquals(false, Palindrome.palindromeForLoop("Abba"));
-        assertEquals(true, Palindrome.palindromeForLoop("abba"));
+        assertFalse(Palindrome.palindromeForLoop("Abba"));
+        assertTrue(Palindrome.palindromeForLoop("abba"));
+    }
+
+    @Test
+    public void palindromeOptimised() {
+        assertFalse(Palindrome.palindromeOptimised("Abba"));
+        assertTrue(Palindrome.palindromeOptimised("abba"));
+        assertFalse(Palindrome.palindromeOptimised("rotOr"));
+        assertTrue(Palindrome.palindromeOptimised("RotoR"));
     }
 
     @Test
     public void palindromeJavaScriptSplitJoin() throws Exception {
-        assertEquals(false, palindromeJavaScript("Abba", "palindromeSplitJoin"));
-        assertEquals(true, palindromeJavaScript("abba", "palindromeSplitJoin"));
-        assertEquals(false, palindromeJavaScript("rotOr", "palindromeSplitJoin"));
-        assertEquals(true, palindromeJavaScript("RotoR", "palindromeSplitJoin"));
+        assertFalse(palindromeJavaScript("Abba", "palindromeSplitJoin"));
+        assertTrue(palindromeJavaScript("abba", "palindromeSplitJoin"));
+        assertFalse(palindromeJavaScript("rotOr", "palindromeSplitJoin"));
+        assertTrue(palindromeJavaScript("RotoR", "palindromeSplitJoin"));
     }
 
     @Test
     public void palindromeJavaScriptEvery() throws Exception {
-        assertEquals(false, palindromeJavaScript("Abba", "palindromeEvery"));
-        assertEquals(true, palindromeJavaScript("abba", "palindromeEvery"));
-        assertEquals(false, palindromeJavaScript("rotOr", "palindromeEvery"));
-        assertEquals(true, palindromeJavaScript("RotoR", "palindromeEvery"));
+        assertFalse(palindromeJavaScript("Abba", "palindromeEvery"));
+        assertTrue(palindromeJavaScript("abba", "palindromeEvery"));
+        assertFalse(palindromeJavaScript("rotOr", "palindromeEvery"));
+        assertTrue(palindromeJavaScript("RotoR", "palindromeEvery"));
     }
 
     private static boolean palindromeJavaScript(String str, String functionName) throws Exception {
